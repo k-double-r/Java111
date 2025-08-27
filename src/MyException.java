a3e6bfc130f876f85ba7511283cc930d7a4939c8
krris <krrishkumar.0810@gmail.com>
2025-08-27 14:24:07 +0530

    27 aug 2025

---
+++ src/MyException.java
@@ -0,0 +1,33 @@
+import java.io.DataInputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MyException {
+    public static void main(String[] args)throws Exception {
+        int x, y, z=0;
+        DataInputStream dis = new DataInputStream(System.in);
+        x = dis.readInt();
+        y = dis.readInt();
+        z=x/y;
+        System.out.println(z);
+//      Scanner sc = new Scanner(System.in);
+//        try{
+//            x = sc.nextInt();
+//            y = sc.nextInt();
+//            z = x/y;
+//            System.out.println("Exception does not occur");
+//        }
+//        catch(ArithmeticException e){
+//            System.out.println(e.getMessage());
+//        }
+//        catch(InputMismatchException e){
+//            System.out.println(e);
+//        }
+//        catch(Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//        finally{
+//            System.out.println("Result is : "+z);
+//        }
+      }
+}
