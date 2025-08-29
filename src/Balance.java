bf38da36bec6b4cd1aadd7e3f0385315e6302d09
krris <krrishkumar.0810@gmail.com>
2025-08-29 11:42:50 +0530

    27 aug 2025, Exception

---
+++ src/Balance.java
@@ -0,0 +1,5 @@
+public class Balance extends RuntimeException{
+    public Balance(String message){
+        super(message);
+    }
+}
---
+++ src/Bank.java
@@ -0,0 +1,12 @@
+public class Bank {
+    private int balanc=1000;
+    public void withdraw(int amount){
+        if(amount>balanc){
+            throw new Balance("Cannot Withdraw");
+        }
+        else{
+            balanc=balanc-amount;
+            System.out.println("Withdraw Successful");
+        }
+    }
+}
