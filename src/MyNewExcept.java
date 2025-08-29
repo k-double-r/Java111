38969614fcb22382de534bb20c6040a9136d2164
krris <krrishkumar.0810@gmail.com>
2025-08-29 11:27:34 +0530

    27 aug 2025, Exception

---
+++ src/MyNewExcept.java
@@ -0,0 +1,11 @@
+public class MyNewExcept {
+    public static void main(String[] args) {
+        UserException ud = new UserException();
+        try{
+            ud.validate(18);
+        }
+        catch(InvalidAge e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/UserException.java
@@ -0,0 +1,10 @@
+public class UserException {
+    public void validate(int age) throws InvalidAge{
+        if(age<18){
+            throw new InvalidAge("Cannot vote");
+        }
+        else{
+            System.out.println("Can vote");
+        }
+    }
+}
