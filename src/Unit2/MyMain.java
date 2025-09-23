d3079daa5e62d52fb5bdad0ef99d073f37cb9fe1
krris <krrishkumar.0810@gmail.com>
2025-09-23 12:23:31 +0530

    23 Sept 2025

--- src/Unit2/MyMain.java
+++ src/Unit2/MyMain.java
@@ -3,12 +3,14 @@ package Unit2;
 public class MyMain {
     public static void main(String[] args) {
         Threading thi = new Threading();
+        Thread2 th2 = new Thread2();
         thi.start();
-        System.out.println(thi.getName());
-        thi.setName("KIET Thread");
-        System.out.println(thi.getName());
-        System.out.println(thi.getPriority());
-        thi.setPriority(9);
-        System.out.println(thi.getPriority());
+        th2.start();
+//        System.out.println(thi.getName());
+//        thi.setName("KIET_Thread");
+//        System.out.println(thi.getName());
+//        System.out.println(thi.getPriority());
+//        thi.setPriority(9);
+//        System.out.println(thi.getPriority());
     }
 }
--- src/Unit2/Thread2.java
+++ src/Unit2/Thread2.java
@@ -1,14 +1,14 @@
 package Unit2;
 import java.util.*;
-public class Threading extends Thread{
+public class Thread2 extends Thread{
     public void run() {
         for(int i=0;i<10;i++) {
             try{
-                Thread.sleep(1000);
+                Thread.sleep(2500);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-            if(i%2==1) {
+            if(i%2==0) {
                 System.out.println(i);
             }
         }
